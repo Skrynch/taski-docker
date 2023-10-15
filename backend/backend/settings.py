d8acbd1aa2aa5609de488a393ea72792e4a2391c
Илья Зотов <dubkibobri@yandex.ru>
2023-10-15 11:12:27 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,13 +60,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-	'default': {
-	'ENGINE': 'django.db.backends.postgresql',
-	'NAME': os.getenv('POSTGRES_DB', 'django'),
-	'USER': os.getenv('POSTGRES_USER', 'django'),
-	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-	'HOST': os.getenv('DB_HOST', ''),
-	'PORT': os.getenv('DB_PORT', 5432)
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
